a2159eb2eb729cbcd54a92d465b156c02f44cc51
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-06-28 16:59:58 -0500

    finished customer page

--- src/components/CustomerPage/CustomerForm/CustomerForm.js
+++ src/components/CustomerPage/CustomerForm/CustomerForm.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react'; 
-import { connect } from 'react-redux';
-
-const mapReduxToProps = (reduxStore) => ({
-    customers: reduxStore.customerReducer
-});
 
 class CustomerForm extends Component {
-
     constructor () {
         super();
         this.state = {
@@ -20,90 +14,59 @@ class CustomerForm extends Component {
     
     handleChange = (event) => {
         this.setState({
-            name : event.target.value, 
-            street_address : event.target.value, 
-            city : event.target.value, 
-            zip : event.target.value, 
-            type : event.target.value, 
-        })
+            [event.target.name]: event.target.value, 
+        });
     }
 
-    handleSubmit = () => {
+    handleSubmit = (event) => {
         event.preventDefault();
+        this.props.sendCustomerToRedux(this.state);
         console.log(this.state);
     }
 
-    sendCustomerToRedux = () => {
-        const body = {name: this.state.name, 
-                    street_address: this.state.street_address, 
-                    city: this.state.city, 
-                    zip: this.state.zip, 
-                    type: this.state.type};
-        const action = {type: 'ADD_CUSTOMER', payload: body};
-        this.props.dispatch(action);
-        console.log(body);
-        
-    }
+    ///Give to johnny
+   
 
     render() {
         console.log('rendering CustomerForm');
         
         return (
             <div>
-
-            <form onSubmit={this.handleSubmit}> 
-                <div className="field">
-                    <input className="input" type="text" name="name" placeholder="Name"
-                            value={this.state.name} onChange={this.handleChange}> 
-                    </input>
-                </div>
-
-                 <div className="field">
-                    <input className="input" type="text" name="street_address" placeholder="Street Address"
-                            value={this.state.street_address} onChange={this.handleChange}> 
-                    </input>
-                </div>
-
-                <div className="field">
-                    <input className="input" type="text" name="city" placeholder="City"
-                            value={this.state.city} onChange={this.handleChange}> 
-                    </input>
-                </div>
-
-                <div className="field">
-                    <input className="input" type="text" name="zip" placeholder="Zip"
-                            value={this.state.zip} onChange={this.handleChange}> 
-                    </input>
-                </div>
-
-                <div className="field">
-                {/* <div className="control"> */}
-                 <label className="radio">
-                    <input className="radio" type="radio" name="Pickup" 
-                            value="Pickup" checked={this.state.type === "Pickup"} onChange={this.handleChange} /> 
-                            Pickup
-                </label>
-
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <input onChange={this.handleChange} placeholder="Name" 
+                        value={this.state.name} name="name" />
+                    </div>
+                <div>
+                    <input onChange={this.handleChange} placeholder="Street Address" 
+                        value={this.state.street_address} name="street_address" />
+                    </div>
+                <div>
+                    <input onChange={this.handleChange} placeholder="City" 
+                        value={this.state.city} name="city" />
+                    </div>
+                <div>
+                    <input onChange={this.handleChange} placeholder="Zip" 
+                        value={this.state.zip} name="zip" />
+                    </div>
+                <div>
                 <label className="radio">
-                    <input className="radio" type="radio" name="Delivery" 
-                            value={this.state.type} onChange={this.handleChange} /> 
-                            Delivery
-                </label>
-                {/* </div> */}
-                </div>
-
-
-
-
-
-                
-
+                    <input onChange={this.handleChange} className="radio" type="radio"  
+                        value={this.state.type === "Pickup"} name="type" />
+                        Pickup
+                    </label>
+                    </div>
+                      <div>
+                <label className="radio">
+                    <input onChange={this.handleChange} className="radio" type="radio"  
+                        value={this.state.type === "Delivery"} name="type" />
+                       Delivery
+                    </label>
+                    </div>
             </form>
-            </div>
-
-
+        </div>
         );
     }
 }
 
-export default connect(mapReduxToProps) (CustomerForm);
+export default CustomerForm;
